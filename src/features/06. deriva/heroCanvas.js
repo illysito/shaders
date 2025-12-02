8676e588c6eb96371a35f2a6db715b7283d3a980
illysito <illyalukyanovdesign@gmail.com>
2025-12-02 12:25:57 +0000

    Update heroCanvas.js

--- src/features/06. deriva/heroCanvas.js
+++ src/features/06. deriva/heroCanvas.js
@@ -249,7 +249,7 @@ function canvas() {
 
       vec4 distortionMap = texture2D(uDistortionMap, noiseUv);
       vec4 distortionMap2 = texture2D(uDistortionMap2, noiseUv);
-      vec4 distortionMapFinal = mix(distortionMap, distortionMap2, uSw);
+      vec4 distortionMapFinal = mix(distortionMap2,distortionMap, uSw);
       float distMap = distortionMapFinal.r;
 
       // AURA NOISE
@@ -278,7 +278,7 @@ function canvas() {
 
       vec4 tex1 = texture2D(uTexture, noiseUv);
       vec4 tex2 = texture2D(uTexture2, noiseUv);
-      vec4 tex = mix(tex1, tex2, uSw);
+      vec4 tex = mix(tex2, tex1, uSw);
       vec3 color = tex.rgb;
 
       // // ADDITIVE BLEND
