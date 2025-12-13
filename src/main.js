c449b11ea270b4bb567c0070fa99b4c86d68ba96
illysito <illyalukyanovdesign@gmail.com>
2025-12-13 21:14:11 +0000

    :)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import textureUI from './features/01. texture loading/texture_ui'
-import dispUI from './features/02. displacement/disp_ui'
-import mouseDispUI from './features/03. mouse-displacement/mouseDisp_ui'
-import heroDispUI from './features/04. hero-displacement/heroDisp_ui'
-import golUI from './features/05. game of life/gol_ui'
+// import textureUI from './features/01. texture loading/texture_ui'
+// import dispUI from './features/02. displacement/disp_ui'
+// import mouseDispUI from './features/03. mouse-displacement/mouseDisp_ui'
+// import heroDispUI from './features/04. hero-displacement/heroDisp_ui'
+// import golUI from './features/05. game of life/gol_ui'
 //-----------//
 import countdown from './features/06. deriva/countdown'
 import heroCanvas from './features/06. deriva/heroCanvas'
@@ -13,23 +13,23 @@ import asciiCanvas from './features/07. ascii/asciiCanvas'
 
 import './styles/style.css'
 
-function runShaderFunctions() {
-  const textureCanvas = document.querySelector('#texture-canvas')
-  const displacementCanvas = document.querySelector('#displacement-canvas')
-  const mouseDisplacementCanvas = document.querySelector(
-    '#mouse-displacement-canvas'
-  )
-  const heroDisplacementCanvas = document.querySelector(
-    '#hero-displacement-canvas'
-  )
-  const golCanvas = document.querySelector('#gol-canvas')
+// function runShaderFunctions() {
+//   const textureCanvas = document.querySelector('#texture-canvas')
+//   const displacementCanvas = document.querySelector('#displacement-canvas')
+//   const mouseDisplacementCanvas = document.querySelector(
+//     '#mouse-displacement-canvas'
+//   )
+//   const heroDisplacementCanvas = document.querySelector(
+//     '#hero-displacement-canvas'
+//   )
+//   const golCanvas = document.querySelector('#gol-canvas')
 
-  textureUI(textureCanvas)
-  dispUI(displacementCanvas)
-  mouseDispUI(mouseDisplacementCanvas)
-  heroDispUI(heroDisplacementCanvas)
-  golUI(golCanvas)
-}
+//   textureUI(textureCanvas)
+//   dispUI(displacementCanvas)
+//   mouseDispUI(mouseDisplacementCanvas)
+//   heroDispUI(heroDisplacementCanvas)
+//   golUI(golCanvas)
+// }
 
 function runDerivaFunctions() {
   countdown()
@@ -46,6 +46,6 @@ function runAsciiFunctions() {
   asciiCanvas()
 }
 
-if (document.body.classList.contains('shaders__body')) runShaderFunctions()
+// if (document.body.classList.contains('shaders__body')) runShaderFunctions()
 if (document.body.classList.contains('deriva__body')) runDerivaFunctions()
 if (document.body.classList.contains('ascii__body')) runAsciiFunctions()
